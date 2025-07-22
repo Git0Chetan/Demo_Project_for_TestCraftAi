84aa71aefae18a749fef0eb91c3a8c1b15ec0eae
Chetan Patil <chetan.patil@worldline.com>
2025-07-22 12:04:49 +0530

    remove some code

--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,4 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-
-    public int multi(int a,int b){
-        return a*b;
-    }
-
-    public int div(int a,int b){
-        return a/b;
-    }
 }
